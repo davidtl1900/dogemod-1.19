9df288bbaa093f9b81d6d2150136dd640ab18f97
David Loto'aniu <davidtl1900@gmail.com>
2022-11-08 20:26:01 -0700

    Initial commit

---
+++ src/main/java/net/tevolo/dogemod/DogeModClient.java
@@ -0,0 +1,10 @@
+package net.tevolo.dogemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DogeModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
